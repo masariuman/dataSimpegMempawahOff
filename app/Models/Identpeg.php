b1b87b2c2decda4801694d4424ae55f68784cb5c
masariuman <masariuman@gmail.com>
2022-08-30 14:04:42 +0700

    master

---
+++ app/Models/Identpeg.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Identpeg extends Model
+{
+    protected $table = 'identpeg';
+    // protected $fillable = [
+    //     'rinku',
+    //     'pegawai_id',
+    //     'alamat',
+    //     'telepon',
+    //     'handphone',
+    //     'emailDinas',
+    //     'emailPribadi',
+    //     'nik',
+    //     'nomorKK',
+    //     'agama_id',
+    //     'lokasiKerja',
+    //     'npwp',
+    //     'tanggalNpwp',
+    //     'bpjs',
+    //     'karis',
+    //     'taspen',
+    //     'tanggalTaspen',
+    //     'tapera',
+    //     'kppn',
+    //     'kelasJabatan',
+    //     'sutattsu',
+    //     'akta',
+    //     'created_at',
+    //     'updated_at'
+    // ];
+}
